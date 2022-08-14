04ce6a72006c4f3d1eaa3db986b123854e98a1ad
thebuya <108401506+thebuya@users.noreply.github.com>
2022-08-15 00:26:08 +0300

    Initial commit

---
+++ Main Hero/js/script.js
@@ -0,0 +1,10 @@
+
+let collectorsign = document.getElementById("collectorsign")
+
+function opencollectorsign(){
+    collectorsign.classList.add("open-collectorsign");
+}
+function closecollectorsign(){
+    collectorsign.classList.remove("open-collectorsign");
+}
+
